d0052dcf7b8c18a09a186d0fdd1269575222e9b1
Dmitriy Mayboroda <dmytromay@wix.com>
2020-07-03 12:16:24 +0300

    Change project structure in order to support more flexible way for external dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,10 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5" % "test"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "1.0.0"
+
+  lazy val doobieCore = "org.tpolecat" %% "doobie-core" % "0.8.8"
+  lazy val doobieH2 = "org.tpolecat" %% "doobie-h2" % "0.8.8"
+  lazy val doobieTest = "org.tpolecat" %% "doobie-scalatest" % "0.8.8" % "test"
 }
--- src/main/scala/com/wix/ocicat/storage/ThrottlerCapacity.scala
+++
@@ -1,3 +0,0 @@
-package com.wix.ocicat.storage
-
-case class ThrottlerCapacity[A](key: A, counts: Int)
